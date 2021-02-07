abebca7efce4ed3907a83ccbe8d286e76590a2cf
Kenny <3454741+WarriorOfWire@users.noreply.github.com>
2021-02-06 23:06:28 -0800

    Add SEH and readme docs
    Initial commit of sequential exponential histogram MVP after making
    some dashboard panels and performance testing a few scenarios.
    Remember to mark your pure functions IMMUTABLE and PARALLEL SAFE
    if you want them to be quick!
    
    Continuous Aggregates see a significant performance regression
    compared to equivalent bare hypertables on SEH aggregations.

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # SEH
 Sequential Exponential Histograms for TimescaleDB
+
+
+## About Sequential Exponential Histogram downsampling
+Keep your dimensions and an idea of your measurements' behavior even when bucketed into very wide buckets.
+Sequential Exponential Histograms (SEH) are jsonb and are sparse. If your service tends to be fairly consistent
+or even consistently modal, your histograms will tend to be compact. If your measurements are random across a
+huge spread, your histograms will be somewhat larger; they grow at the rate of:
+`2 * log10(measurement cardinality per window)`
+So if your service is slow during business hours and fast at night you will have different buckets predominantly
+populated at different times, but no 0 buckets. The histograms are sparse, densely packed.
+They are also re-combinable across grouping dimensions.
+
+You can record a histogram per-(region, server, device) and seamlessly inspect behavior by any of those dimensions.
+
+## Who is this for
+SEH is lossy; however it tries to retain the high level shape of the original input data set. It is for service
+owners who need a more compact representation of their high-frequency fine-grained measurements and who want to
+retrieve better historical insights than answers seeded a priori (like bespoke avg or percentile_disc columns in
+rollups)
+
+
+## Example usage with TimescaleDB Continuous Aggregates
+
+CREATE MATERIALIZED VIEW metrics_1h
+WITH (timescaledb.continuous) AS
+  SELECT
+    time_bucket('1 hour', time) AS time, -- Select columns with the same names. This is a rollup table not a projection table.
+    device_id,
+    region,
+    server,
+    accumulate_seh(report_latency) as report_latency  -- Retain a sequential exponential histogram of report_latency per hour, per device, per region, per server
+  from metrics
+  group by 1, 2, 3, 4;
+select add_continuous_aggregate_policy('metrics_1h', start_offset => interval '10 minutes', end_offset => interval '1 minute', schedule_interval => interval '1 minute');
+
+grant select on metrics_1h to grafana_read_role;  -- You do have a limited role for grafana in your db right?
+
+
+And in Grafana for latency of devices reporting to IAD:
+Heatmap visualization with Data Format "Time series buckets"
+```
+select
+  $__timeGroup(time, $__interval) as time,
+  (buckets(accumulate_seh(report_latency))).*
+from metrics_1h
+where
+  $__timeFilter(time)
+  and region = 'IAD'
+group by 1
+ORDER BY 1
+```
+
+
---
+++ seh.sql
@@ -0,0 +1,75 @@
+
+-- Data type alias for readability
+create domain sequential_exponential_histogram as jsonb;
+
+-- Shared function to glob up a sequential_exponential_histogram from a number column
+CREATE OR REPLACE FUNCTION sequential_exponential_histogram_accumulate(internal_state sequential_exponential_histogram, next_row double precision) RETURNS sequential_exponential_histogram
+AS $fn$
+DECLARE
+    bucket text;
+    floor_log numeric;
+BEGIN
+    IF next_row = 0
+        THEN bucket := '0';
+        ELSE
+            floor_log := POW(10, FLOOR(LOG(10, next_row::numeric)));
+            bucket := (CEIL(next_row * 2 / floor_log) / 2 * floor_log)::text;
+    END IF;
+    IF internal_state ? bucket
+        THEN internal_state := jsonb_set( internal_state, ARRAY[bucket], to_jsonb((internal_state->bucket)::integer + 1) );
+        ELSE internal_state := jsonb_insert( internal_state, ARRAY[bucket], to_jsonb(1) );
+    END IF;
+    return internal_state;
+END;
+$fn$ LANGUAGE plpgsql STRICT IMMUTABLE PARALLEL SAFE;
+
+-- Shared function to combine sequential_exponential_histograms
+CREATE OR REPLACE FUNCTION sequential_exponential_histogram_combine(internal_state sequential_exponential_histogram, next_row sequential_exponential_histogram) RETURNS sequential_exponential_histogram
+AS $fn$
+DECLARE
+    _key   text;
+    _value numeric;
+BEGIN
+    FOR _key, _value IN SELECT * from jsonb_each_text(next_row) LOOP
+        IF internal_state ? _key
+            THEN internal_state := jsonb_set( internal_state, ARRAY[_key], to_jsonb((internal_state->_key)::integer + _value) );
+            ELSE internal_state := jsonb_insert( internal_state, ARRAY[_key], to_jsonb(_value) );
+        END IF;
+    END LOOP;
+    return internal_state;
+END;
+$fn$ LANGUAGE plpgsql STRICT IMMUTABLE PARALLEL SAFE;
+
+-- For downsampling a numeric row into a histogram
+CREATE OR REPLACE AGGREGATE accumulate_seh(double precision)
+(
+    sfunc = sequential_exponential_histogram_accumulate,
+    stype = sequential_exponential_histogram,
+    initcond = '{}',
+    combinefunc = sequential_exponential_histogram_combine,
+    PARALLEL = SAFE
+);
+
+-- For dynamically combining histograms for presentation
+CREATE OR REPLACE AGGREGATE accumulate_seh(sequential_exponential_histogram)
+(
+    sfunc = sequential_exponential_histogram_combine,
+    stype = sequential_exponential_histogram,
+    initcond = '{}',
+    combinefunc = sequential_exponential_histogram_combine,
+    PARALLEL = SAFE
+);
+
+
+-- Pivots a sequential_exponential_histogram out to rows of | bucket int | count text | for convenient graphing in Grafana.
+-- Used in Grafana like:
+-- select
+--   time_bucket('1m', time) as time,
+--   (buckets(accumulate_seh(some_column))).*  -- Can be numeric or a pre-downsampled SEH column
+-- from metrics_table
+-- group by 1 order by 1;
+CREATE OR REPLACE FUNCTION buckets( seh sequential_exponential_histogram ) RETURNS TABLE(bucket text, count numeric)
+AS $$
+    select (a.each).key::numeric::bigint::text as bucket, (a.each).value::numeric as count from (select jsonb_each(seh) as each) a;
+$$ LANGUAGE SQL STRICT IMMUTABLE PARALLEL SAFE;
+
